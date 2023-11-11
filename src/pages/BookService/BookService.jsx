b539ce18b981a247ae9dee4dd92de8270bf098e4
saad200224 <saad19021994@outlook.com>
2023-11-11 13:01:26 +0600

    59-5

--- src/pages/BookService/BookService.jsx
+++ src/pages/BookService/BookService.jsx
@@ -6,7 +6,7 @@ const BookService = () => {
 
     const service = useLoaderData();
     
-    const { title, _id, price } = service;
+    const { title, _id, price, img } = service;
     const { user } =useContext(AuthContext);
 
     const handleBookService = event =>{
@@ -16,14 +16,31 @@ const BookService = () => {
         const name = form.name.value;
         const date = form.date.value;
         const email = user?.email;
-        const order = {
+        const booking = {
             customerName : name,
             email,
+            img,
             date,
-            service: _id,
+            service: title,
+            service_id: _id,
             price: price
         }
-        console.log(order)
+        console.log(booking)
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then (res => res.json())
+        .then( data => {
+            console.log(data)
+            if(data.insertedId){
+                alert('Service book successful.')
+            }
+        })
     }
 
     return (
